1c8eecc5d6c424f1db599af21c9019f4ef8ed362
Matheus Aguiar <matheusaguiar.developer@gmail.com>
2023-10-21 02:00:45 -0400

    mend

--- innyx-frontend/src/App.vue
+++ innyx-frontend/src/App.vue
@@ -1,30 +1,124 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-purple justify-content-center">
+      <a class="navbar-brand" href="#" style="font-family: 'Arial', sans-serif; font-size:60px;">Innyx</a>
   </nav>
-  <router-view/>
+  <router-view id="content" />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* CUSTOM STYLE */
+
+html{
+background:#d0dcff;
+}
+#content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid lightgray;
+  padding: 4rem 4rem;
+  border-radius: 5px;
+  background: #fefefe;
+}
+
+.background {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background: rgb(250, 250, 250);
+}
+
+.navbar {
+  background: #540094;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 80px;
+  box-shadow: 6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028),
+    22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042),
+    100px 100px 80px rgba(0, 0, 0, 0.07);
+}
+
+.nav {
+  width: 80%;
+  min-width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: #fafafa;
+}
+
+#name_project {
+  font-weight: 700;
+}
+
+.containerLogin {
+  width: 400px;
+  max-width: 95%;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+}
+
+.input > label {
+  text-align: start;
+}
+
+.input > input {
+  margin-top: 6px;
+  height: 38px !important;
+}
+
+/* From uiverse.io */
+.btn-pers {
+  position: relative;
+  left: 50%;
+  padding: 1em 2.5em;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 700;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+  transform: translateX(-50%);
+}
+
+.btn-pers:hover {
+  background-color: #800080;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.2);  
+  color: #fff;
+  transform: translate(-50%, -7px);
+}
+
+.btn-pers:active {
+  transform: translate(-50%, -1px);
+}
+
+/*  */
+
+.alternative-option {
   text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.alternative-option > span {
+  color: #0d6efd;
+  cursor: pointer;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+#sign_out {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
