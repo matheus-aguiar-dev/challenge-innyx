f9711a70cb7383b4601b883de32a1bfbc77c55f0
Matheus Aguiar <matheusaguiar.developer@gmail.com>
2023-10-22 18:26:50 -0400

    added pagination and loading components

---
+++ innyx-frontend/src/components/Pagination.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+        <a class="page-link" href="#" @click="changePage(currentPage - 1)">Anterior</a>
+      </li>
+      <li class="page-item" v-for="page in totalPages" :key="page" :class="{ 'active': currentPage === page }">
+        <a class="page-link" href="#" @click="changePage(page)">{{ page }}</a>
+      </li>
+      <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
+        <a class="page-link" href="#" @click="changePage(currentPage + 1)">Próximo</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    totalPages: {
+      type: Number,
+      required: true
+    },
+    currentPage: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    changePage(page) {
+      if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+        this.$emit('page-change', page);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add your pagination styles here */
+</style>
---
+++ innyx-frontend/src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center container mt-5">
+      <i class="fas fa-spinner fa-spin fa-3x"></i> <!-- Font Awesome spinner -->
+      <h1>Carregando...</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+};
+</script>
+
+<style scoped>
+@import "~@fortawesome/fontawesome-free/css/all.css";
+/* Your component styles here */
+</style>
+
